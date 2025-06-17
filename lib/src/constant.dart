77d9a5361af8d86171163cda333169c43035d50b
jak-br <calunsag.br@gmail.com>
2025-06-17 12:04:27 +0800

    Added .env and app.*.symbols to .gitignore.

---
+++ lib/src/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF1EC340);
+const secondaryColor = Color(0xFF424242);
+
+const textColor = Color(0xFF727272);
+const darkColor = Color(0xFF070707);
+const bgColor = Color(0xFFFFFFFF);
--- lib/src/main_app.dart
+++ lib/src/main_app.dart
@@ -1,11 +1,24 @@
+import 'package:ecalunsag_bio/src/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+          primaryColor: primaryColor,
+          scaffoldBackgroundColor: bgColor,
+          canvasColor: bgColor,
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
+              .apply(bodyColor: Colors.white)
+              .copyWith(
+                bodyMedium: TextStyle(color: textColor),
+                bodyLarge: TextStyle(color: textColor),
+              )),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
