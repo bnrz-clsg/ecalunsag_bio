d582ade3f9a9360c47d406acdc7710996b6a4f32
jak-br <e.calunsag12@gmail.com>
2025-06-21 22:53:21 +0800

    Activate download CV

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'src/main_app.dart';
 
-void main() async {
-  await dotenv.load(fileName: ".env");
+void main() {
+  // await dotenv.load(fileName: ".env");
   runApp(const MainApp());
 }
--- lib/src/services/file_downloader.dart
+++ lib/src/services/file_downloader.dart
@@ -1,14 +1,11 @@
 import 'dart:html' as html;
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 class FileDownloadService {
   static void downloadResumeFromDrive({
     String fileName = 'Ebenezer_Calunsag_Resume.pdf',
   }) {
-    final String cv_id = dotenv.env['CV_ID'] ?? 'default';
-    final String gdrive_url = "https://drive.google.com/uc?export=download&id=";
-    final downloadUrl = gdrive_url + cv_id;
+    final downloadUrl =
+        "https://drive.google.com/uc?export=download&id=1QG39VvlzSrnr9AE4YEBlKlPheKN5m9Hb";
 
     final anchor = html.AnchorElement(href: downloadUrl)
       ..target = 'blank'
